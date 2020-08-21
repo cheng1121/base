f80009deed6786841feaecedb90092f13785aa6e
404081792@qq.com <404081792@qq.com>
2020-08-21 13:15:38 +0800

    v0.0.5

--- lib/utils/sp_util.dart
+++ lib/utils/sp_util.dart
@@ -14,4 +14,64 @@ class Sp {
   void _init() async {
     _sharedPreferences = await SharedPreferences.getInstance();
   }
+
+  Future<bool> setString(String key, String value) {
+    return _sharedPreferences.setString(key, value);
+  }
+
+  String getString(String key) {
+    return _sharedPreferences.getString(key);
+  }
+
+  Future<bool> setBool(String key, bool value) {
+    return _sharedPreferences.setBool(key, value);
+  }
+
+  bool getBool(String key) {
+    return _sharedPreferences.getBool(key);
+  }
+
+  Future<bool> setInt(String key, int value) {
+    return _sharedPreferences.setInt(key, value);
+  }
+
+  int getInt(String key) {
+    return _sharedPreferences.getInt(key);
+  }
+
+  Future<bool> setDouble(String key, double value) {
+    return _sharedPreferences.setDouble(key, value);
+  }
+
+  double getDouble(String key) {
+    return _sharedPreferences.getDouble(key);
+  }
+
+  Future<bool> setStringList(String key, List<String> value) {
+    return _sharedPreferences.setStringList(key, value);
+  }
+
+  List<String> getStringList(String key) {
+    return _sharedPreferences.getStringList(key);
+  }
+
+  bool containsKey(String key) {
+    return _sharedPreferences.containsKey(key);
+  }
+
+  get(String key) {
+    return _sharedPreferences.get(key);
+  }
+
+  Set<String> getKeys() {
+    return _sharedPreferences.getKeys();
+  }
+
+  Future<bool> clear() {
+    return _sharedPreferences.clear();
+  }
+
+  Future<bool> remove(String key) {
+    return _sharedPreferences.remove(key);
+  }
 }
